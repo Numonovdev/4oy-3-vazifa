9dedf07cd986163b4029e0a628a516ff91845007
Numonovdev <ebxuzsher@gmail.com>
2024-06-11 02:16:06 +0300

    . finished

---
+++ js/index.js
@@ -0,0 +1,418 @@
+// boolean-1
+
+// let a = prompt('butun son kiriting')
+// if (a > 0){
+//      alert('musbat')
+// } else if (a < 0) {alert('manfi')} else {alert(' bu son 0')}
+
+
+// boolean-3
+
+// let a = prompt('butun son kiriting')
+// if (a % 2 === 0){
+//      console.log('rost')
+// } else {console.log('yolgon')}
+
+// boolean-2
+
+// let a = prompt('butun son kiriting')
+// if (a % 2 === 0){
+//      console.log('yolgon')
+// } else {console.log('rost')}
+
+//   boolean-4
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+// if (a > 2 && b <= 3){
+//      alert('rost')
+//      } else {
+//      alert('yolgon')
+// }
+
+// boolean-5
+
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+// if (a >= 0 || b < -2){
+//      alert('rost')
+//      } else {
+//      alert('yolgon')
+// }
+
+
+// boolean-6
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+// let c = prompt('c sonini kiriting')
+// if (a <=b && b <= c){
+//      alert('rost')
+//      } else {
+//      alert('yolgon')
+// }
+
+// boolean-7
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+// let c = prompt('c sonini kiriting')
+// if (a <=b && b <= c){
+//      alert('rost')
+//      } else if(c <=b && b <= a) {
+//      alert('rost')
+//      } else{
+//           alert('yolgon')
+// }
+
+// boolean-8
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+// if (a % 2 === 1 && b % 2 === 1){
+//      alert('rost')
+//      } else {
+//      alert('yolgon')
+// }
+
+// boolean-9
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+// let c = a += b 
+// if (a % 2 === 1 && b % 2 === 1){
+//      alert('rost')
+// } else if (c % 2 === 1){
+//      alert('rost')
+// } else {
+//      alert('yolgon')
+// }
+
+// boolean-10
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+// let c = a += b 
+//  if (c % 2 === 1){
+//      alert('rost')
+// } else {
+//      alert('yolgon')
+// }
+
+// boolean-11
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+// let c = a += b 
+//  if (c % 2 === 0){
+//      alert('rost')
+// } else {
+//      alert('yolgon')
+// }
+
+// boolean-12
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+// let c = prompt('c sonini kiriting')
+
+// if(a > 0 && b > 0 && c > 0){
+//      alert('rost')
+// }
+
+// boolean-13
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+// let c = prompt('c sonini kiriting')
+
+// if(a > 0 && b > 0 && c > 0){
+//      alert('rost')
+// } else if(a > 0 && b > 0 && c < 0){
+//      alert('rost')
+// } else if(a > 0 && b < 0 && c > 0){
+//      alert('rost')
+// } else if(a > 0 && b < 0 && c < 0){
+//      alert('rost')
+// } else if(a < 0 && b > 0 && c > 0){
+//      alert('rost')
+// } else if(a < 0 && b > 0 && c < 0){
+//      alert('rost')
+// } else if(a < 0 && b < 0 && c > 0){
+//      alert('rost')
+// } else { alert('xato')}
+
+// boolean-14
+
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+// let c = prompt('c sonini kiriting')
+// if( a === 0 || b === 0 || c === 0){
+//      alert(' kiritilgan sonlar orasida nol bor')
+// } else if(a > 0 && b < 0 && c < 0){
+//      alert('rost')
+// } else if(a < 0 && b > 0 && c < 0){
+//      alert('rost')
+// } else if(a < 0 && b < 0 && c > 0){
+//      alert('rost')
+// } else { alert('xato')}
+
+// boolean-15
+
+// let a = prompt('a sonini kiriting')
+// let b = prompt('b sonini kiriting')
+// let c = prompt('c sonini kiriting')
+
+//  if(a > 0 && b > 0 && c < 0){
+//      alert('rost')
+// } else if(a > 0 && b < 0 && c > 0){
+//      alert('rost')
+// } else  if(a < 0 && b > 0 && c > 0){
+//      alert('rost')
+// } else { alert('xato')}
+
+// boolean-16
+// let a = prompt('musbat butun son kiriting')
+
+// if(a > 0){
+//      if( a >= 10 && a <= 99 && a % 2 ===0){
+//           alert('juft')
+//      }
+// }
+
+
+
+// if-1
+
+// let a = prompt('butun son kiriting')
+// let y;
+// if(a > 0){
+//      y = a += 1
+//      alert(y)
+// } else if(a < 0){
+//      alert(a)
+// } else{ alert(' nol')}
+
+// if-2
+
+
+// let a = prompt('butun son kiriting')
+// let y;
+// if(a > 0){
+//      y = a += 1
+//      alert(y)
+// } else if(a < 0){
+//      y = a += 2
+//      alert(a)
+// } else{ alert(' nol')}
+
+// if-3
+
+// let a = prompt('butun son kiriting')
+// let y;
+// if(a > 0){
+//      y = a += 1
+//      alert(y)
+// } else if(a < 0){
+//      y = a += 2
+//      alert(a)
+// } else{ alert(' 10')}
+
+// if-4
+
+// let a = prompt('1-sonni kiriting')
+// let b = prompt('2-sonni kiriting')
+// let c = prompt('3-sonni kiriting')
+
+// if(a > 0 && b > 0 && c > 0){
+//      alert('3ta')
+// } else if(a > 0 && b > 0 && c < 0){
+//      alert('2ta')
+// } else if(a > 0 && b < 0 && c > 0){
+//      alert('2ta')
+// } else if(a > 0 && b < 0 && c < 0){
+//      alert('1ta')
+// } else if(a < 0 && b > 0 && c > 0){
+//      alert('2ta')
+// } else if(a < 0 && b > 0 && c < 0){
+//      alert('1ta')
+// } else if(a < 0 && b < 0 && c > 0){
+//      alert('1ta')
+// } else { alert('xato')}
+
+// if=5
+
+// let a = prompt('1-sonni kiriting')
+// let b = prompt('2-sonni kiriting')
+// let c = prompt('3-sonni kiriting')
+
+// if(a > 0 && b > 0 && c > 0){
+//      alert('3ta')
+// } else if(a > 0 && b > 0 && c < 0){
+//      alert('2ta musbat 1ta manfi')
+// } else if(a > 0 && b < 0 && c > 0){
+//      alert('2ta musbat 1ta manfi')
+// } else if(a > 0 && b < 0 && c < 0){
+//      alert('1ta musbat 2ta manfi')
+// } else if(a < 0 && b > 0 && c > 0){
+//      alert('2ta musbat 1ta manfi')
+// } else if(a < 0 && b > 0 && c < 0){
+//      alert('1ta musbat 2ta manfi')
+// } else if(a < 0 && b < 0 && c > 0){
+//      alert('1ta musbat 2ta manfi')
+// } else { alert('xato 0 kiritgansz')}
+
+// if-6
+
+// let a = prompt('1-sonni kiriting')
+// let b = prompt('2-sonni kiriting')
+
+// if(a > b){
+//      alert(a,'1-son katta')
+// }else {alert(b,'2-son katta')}
+
+// if-7 tushunmadim
+
+// if-8
+
+// let a = prompt('1-sonni kiriting')
+// let b = prompt('2-sonni kiriting')
+
+// if(a > b){
+//      alert(a,b)
+// }else if(a < b) {alert(b, a)
+// } else{ alert('ikkisi teeng')}
+
+// if-9
+
+// let a = prompt('a-sonni kiriting')
+// let b = prompt('b-sonni kiriting')
+// let c;
+// if(a > b){
+//      c = a;
+//      a = b;
+//      b = c;
+//      alert(a,b)
+// }else if(a < b) {alert(a,b)
+// } else{ alert('ikkisi teeng')}
+
+// if-10
+
+// let a = prompt('a-sonni kiriting')
+// let b = prompt('b-sonni kiriting')
+// let c = a += b;
+// if(a > b){
+//      a = c;
+//      b = c;
+//      alert(a,b)
+// }else if(a < b) {
+//      a = c;
+//      b = c;
+//      alert(a,b)
+// } else{
+//      a = 0
+//      b = 0 
+//      alert(a,b)
+// }
+
+// if-11
+
+// let a = prompt('a-sonni kiriting')
+// let b = prompt('b-sonni kiriting')
+// let c;
+// if(a > b){
+//      c = a;
+//      b = c;
+//      alert(a,b)
+// }else if(a < b) {
+//      c = b;
+//      a = c;
+//      alert(a,b)
+// } else{
+//      a = 0
+//      b = 0 
+//      alert(a,b)
+// }
+
+// if-12
+
+// let a = prompt('1-sonni kiriting')
+// let b = prompt('2-sonni kiriting')
+// let c = prompt('3-sonni kiriting')
+
+// if(a < b && b < c){
+//      alert('kichik son',a)
+// } else if(a < c && c < b){
+//      alert('kichik son ',a)
+// } else if(a > c && c > b){
+//      alert('kichik son ',b)
+// } else if(c > a && a > b){
+//      alert('kichik son ',b)
+// } else if(c < a && a < b){
+//      alert('kichik son ',c)
+// } else if(c < b && b < a){
+//      alert('kichik son ',c)
+// }
+
+// if-13
+
+// let a = prompt('1-sonni kiriting')
+// let b = prompt('2-sonni kiriting')
+// let c = prompt('3-sonni kiriting')
+
+// if(a < b && b < c){
+//      alert('ortasidagi son ',b)
+// } else if(a < c && c < b){
+//      alert('ortasidagi son  ',c)
+// } else if(a > c && c > b){
+//      alert('ortasidagi son  ',c)
+// } else if(c > a && a > b){
+//      alert('ortasidagi son ',a)
+// } else if(c < a && a < b){
+//      alert('ortasidagi son ',a)
+// } else if(c < b && b < a){
+//      alert('ortasidagi son  ',b)
+// }
+
+// if-14
+
+// let a = prompt('1-sonni kiriting')
+// let b = prompt('2-sonni kiriting')
+// let c = prompt('3-sonni kiriting')
+
+// if(a < b && b < c){
+//      alert(a,b,c)
+// } else if(a < c && c < b){
+//      alert('kichik son 'a,c,b)
+// } else if(a > c && c > b){
+//      alert('kichik son 'b,c,a)
+// } else if(c > a && a > b){
+//      alert('kichik son 'b,a,c)
+// } else if(c < a && a < b){
+//      alert('kichik son 'c,a,b)
+// } else if(c < b && b < a){
+//      alert('kichik son 'c,b,a)
+// }
+
+// if-15
+
+
+// let a = prompt('1-sonni kiriting')
+// let b = prompt('2-sonni kiriting')
+// let c = prompt('3-sonni kiriting')
+
+// if(a < b && b < c){
+//      alert(b,c)
+// } else if(a < c && c < b){
+//      alert('kichik son 'c,b)
+// } else if(a > c && c > b){
+//      alert('kichik son 'c,a)
+// } else if(c > a && a > b){
+//      alert('kichik son 'a,c)
+// } else if(c < a && a < b){
+//      alert('kichik son 'a,b)
+// } else if(c < b && b < a){
+//      alert('kichik son 'b,a)
+// }
